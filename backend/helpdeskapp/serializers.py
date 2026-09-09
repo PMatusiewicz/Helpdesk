0ae5d412922d6a10e6a739395c335e82a02ffbb3
PMatusiewicz <mprzemek22@gmail.com>
2026-09-10 00:46:03 +0200

    Registration serializer and view added

---
+++ backend/helpdeskapp/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ["username", "email", "password", "confirm_password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["confirm_password"]:
+            raise serializers.ValidationError({"confirm_password": "Hasła się nie zgadzają"})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop("confirm_password")
+        user = User.objects.create_user(**validated_data)
+        return user
--- backend/helpdeskapp/urls.py
+++ backend/helpdeskapp/urls.py
@@ -1,7 +1,9 @@
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from django.urls import path
+from .views import RegisterView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view()),
-    path('token/refresh/', TokenRefreshView.as_view())
+    path('token/refresh/', TokenRefreshView.as_view()),
+    path('register/', RegisterView.as_view())
 ]
