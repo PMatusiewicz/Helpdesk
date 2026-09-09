270885eebe5429be260e75bc77a1e8eb3c3f7c48
PMatusiewicz <mprzemek22@gmail.com>
2026-09-09 21:55:20 +0200

    Added JWT tokens and seeding the priorities and settings table migration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,3 +140,13 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
--- backend/helpdeskapp/admin.py
+++ backend/helpdeskapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Report)
+admin.site.register(Comment)
+admin.site.register(Attachment)
+admin.site.register(ReportHistory)
+admin.site.register(Priorities)
+admin.site.register(Settings)
